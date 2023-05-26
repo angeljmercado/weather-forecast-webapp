ac6ab71a7d2228110d82a992365a7c3b247d7e1d
angeljmercado <mercadomilletangel@gmail.com>
2023-05-26 16:36:01 -0700

    Convert raw API temperature data to Fahrenheit and Celsius

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 
@@ -12,9 +13,23 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-dates = ["2022", "2021", "2020"]
-temperatures = [10, 11, 15]
-temperatures = [days * i for i in temperatures]
+if place:
+    try:
+        filtered_data = get_data(place, days)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] for dict in filtered_data]
+            fahrenheit_temps = [int(temperature) / 10 * 1.8 + 32 for temperature in temperatures]
+            #celcius_temps = [int(temperature) / 10 for temperature in temperatures]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=fahrenheit_temps, 
+            labels={"x":"Date", "y":"Temperatures(F)"})
+            st.plotly_chart(figure)
 
-figure = px.line(x=dates, y=temperatures, labels={"x":"Date", "y":"Temperatures(C)"})
-st.plotly_chart(figure)
+        if option == "Sky":
+            weather_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            images = {"Clear":"images/clear.png", "Clouds":"images/cloud.png", 
+            "Rain":"images/rain.png", "Snow":"images/snow.snow.png"}
+            weather_images = [images[condition] for condition in weather_conditions]
+            st.image(weather_images, width=115)
+    except KeyError:
+        st.error("The value you entered doesn't exist")
